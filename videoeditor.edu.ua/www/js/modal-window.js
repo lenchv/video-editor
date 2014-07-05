1f51272afd6a752fa4813ba4f1c2280e4afb897d
lenchv <lenchvov@rambler.ru>
2014-07-05 14:56:04 +0300

    video-editor
    Весь проект с Code Igniter'ом

---
+++ videoeditor.edu.ua/www/js/modal-window.js
@@ -0,0 +1,93 @@
+﻿
+//Структура описывающая информацию о файле
+var fileInfo = {
+	name : '_',
+	dir : './videofiles/',
+	typeMIME: ''
+};
+//Структура с костылями для выделения файла
+var checkFile = {
+  currentSet: false,
+  id : 'checked-file',
+  ok: true
+};
+//Прорисовка модального окна для событий
+document.addEventListener('click', containerEvent, false);
+function containerEvent(event){
+//Если нажаты кнопки добавления, удаления или переименования файла, то вывести модальное
+  if(event.target.id == 'button-add' || event.target.id == 'button-delete' || event.target.id == 'button-edit') {
+    var blockWindow = document.createElement('div');
+    var inputWindow = document.createElement('div');
+	var butClose = document.createElement('div');
+	butClose.className = 'close';
+	butClose.innerHTML = 'x'; //заменить на спрайт закрытия окна
+	//Вывод формы в окно в зависимости от выбранной задачи
+	if(event.target.id == 'button-add')
+	{
+		inputWindow.innerHTML = "<div id = 'addfile'>\
+			<form action='http://videoeditor.edu.ua/index.php/main/do_upload'\
+			method='post' accept-charset='utf-8' enctype='multipart/form-data'>\
+			<input type = 'file' name = 'userfile' size = '100000' />\
+			<input type = 'submit' value = 'Load' />\
+			</form> </div>"
+	} else if(event.target.id == 'button-delete')
+	{
+		inputWindow.innerHTML = "<div id = 'delfile'>\
+			<p>Вы уверены, что хотите удалите файл?<\p>\
+			<form action='http://videoeditor.edu.ua/index.php/main/do_delete/"+fileInfo.name+"'\
+			method='post' accept-charset='utf-8' enctype='multipart/form-data'>\
+			<input type = 'submit' value = 'OK'/>\
+			<input type = 'reset' value = 'Отмена' onclick = 'closeWindow()'/>\
+			</form></div>"
+	} else if(event.target.id == 'button-edit' && fileInfo.name != '_')
+	{
+		inputWindow.innerHTML = "<div id = 'delfile'>\
+			<form action='http://videoeditor.edu.ua/index.php/main/do_rename/"+fileInfo.name+"'\
+			method='post' accept-charset='utf-8' enctype='multipart/form-data'>\
+			<label for='new_name'>Введите имя</label><input type = 'text' name = 'new_name' value"+fileInfo.name+"'/>\
+			<input type = 'submit' value = 'Переименовать'/>\
+			</form></div>"
+	} else {
+		closeWindow();		//нужно если в переименовании не выбран файл
+	}
+    blockWindow.classList.add('modal');
+    inputWindow.classList.add('window');
+	inputWindow.appendChild(butClose);
+    blockWindow.appendChild(inputWindow);
+    document.body.appendChild(blockWindow);
+  }
+  //закрытие модального окна, если нажать на крестик или в свободную область
+  if(event.target.className == 'close' || event.target.className == "modal"){
+	closeWindow();
+  }
+  //Выделение файла по клику мыши
+  if(event.target.className == 'file-name')
+  {
+	checkFile.ok = true;
+	if(checkFile.currentSet)
+	{
+		var old = document.getElementById(checkFile.id);
+		if(old.id == event.target.id)
+		{
+			checkFile.ok = false;
+		}
+		old.id = '';
+		fileInfo.name = '_';
+		checkFile.currentSet = false;
+		removeVideo();
+	} 
+	if (checkFile.ok)
+	{
+		checkFile.currentSet = true;
+		event.target.id = checkFile.id;
+		fileInfo.name = event.target.innerHTML;
+		fileInfo.typeMIME = document.getElementById(fileInfo.name).innerHTML;
+		fileInScreen();
+	}
+  }
+}
+//функция закрытия окна
+function closeWindow(){
+	var w = document.getElementsByClassName("modal")[0];
+	w.parentNode.removeChild(w);
+}
---
+++ videoeditor.edu.ua/www/js/player.js
@@ -0,0 +1,72 @@
+﻿/*document.addEventListener('dblclick', getMonitor, false);
+function getMonitor(event) {
+  var element = document.getElementsByTagName('video')[0];
+  if(event.target == element)
+  {
+	if(element.requestFullscreen) {
+		alert(1);
+		element.requestFullscreen();
+	} else if(element.webkitrequestFullscreen) {
+		alert(2);
+		element.webkitRequestFullscreen();
+	} else if(element.mozRequestFullscreen) {
+	alert(1);
+		element.mozRequestFullScreen();
+	}
+  }
+}*/
+
+function fileInScreen() {
+  var screen = document.getElementById('monitor');
+  var video = document.createElement('video');
+  var source = document.createElement('source');
+  var codec;
+  source.setAttribute('src',fileInfo.dir+fileInfo.name);
+  video.setAttribute('onerror','failed(event)');
+  video.controller = new MediaController(); //media controller
+  switch(fileInfo.typeMIME) {
+    case 'video/mp4' : codec = 'avc1.42E01E,mp4a.40.2'; break;
+    case 'video/webm' : codec = 'vp8,theora'; break;
+    case 'video/ogg' : codec = 'theora,vorbis'; break;
+	default: codec = '';
+  }
+  if (!!codec) {
+	source.setAttribute('type',fileInfo.typeMIME+"; codecs = '"+codec+"'");
+	video.appendChild(source);
+	screen.appendChild(video);
+	//alert(video.canPlayType(fileInfo.typeMIME+"; codecs = '"+codec+"'"));
+  }
+}
+
+function removeVideo(){
+  var video = document.getElementsByTagName('video')[0];
+  video.parentNode.removeChild(video);
+}
+
+function noVideo() {
+  var err = document.createElement('div');
+  err.className = 'no-video';
+  err.innerHTML = 'no video';
+  return err;
+}
+
+function failed(e) {
+   // video playback failed - show a message saying why
+   switch (e.target.error.code) {
+     case e.target.error.MEDIA_ERR_ABORTED:
+       alert('You aborted the video playback.');
+       break;
+     case e.target.error.MEDIA_ERR_NETWORK:
+       alert('A network error caused the video download to fail part-way.');
+       break;
+     case e.target.error.MEDIA_ERR_DECODE:
+       alert('The video playback was aborted due to a corruption problem or because the video used features your browser did not support.');
+       break;
+     case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+       alert('The video could not be loaded, either because the server or network failed or because the format is not supported.');
+       break;
+     default:
+       alert('An unknown error occurred.');
+       break;
+   }
+ }
