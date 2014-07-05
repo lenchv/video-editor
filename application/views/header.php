9ac3339c7ef21d65394e252e3e29a8394611d817
lenchv <lenchvov@rambler.ru>
2014-07-05 15:04:04 +0300

    deleted

--- application/views/header.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-  <meta http-equiv=Content-Type content="text/html; charset=windows-1251">
-  <title>video editor v.0.0.1</title>
-  <link href='\css\style.css' rel='stylesheet' type='text/css'>
-  <script type="text/javascript" src="\js\hint.js" charset="utf-8"></script>
-   <script type="text/javascript" src="\js\modal-window.js" charset="utf-8"></script>
-</head>
-<body>
